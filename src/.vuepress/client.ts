da495a8d47a7c1cb221974f2830158d8c1a65aaa
Howson Luo <1029143329@qq.com>
2026-03-12 17:23:13 +0800

    test: 自定义布局示例

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -7,7 +7,13 @@ import { defineClientConfig } from "vuepress/client";
 import AntdvNext from "antdv-next";
 import "antdv-next/dist/antd.css";
 
+// 自定义布局（后续再测试）
+// import Layout from "./layouts/Layout.vue";
+
 export default defineClientConfig({
+  // layouts: {
+  //   Layout
+  // },
   enhance({ app }) {
     // 注册AntdV-next
     app.use(AntdvNext);
---
+++ src/.vuepress/layouts/Layout.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Layout } from "vuepress-theme-hope/client";
+</script>
+
+<!-- <template>
+    <Layout>
+        通过 contentBefore 插槽在 Markdown 内容前添加广告
+        <template #contentBefore>
+            <div>广告内容</div>
+        </template>
+        使用 pageBottom 插槽引入评论组件
+        <template #pageBottom>
+            <CommentService />
+        </template>
+    </Layout>
+</template> -->
