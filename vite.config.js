4c0ae2408d2840df8ac978ebfb2239b022412e85
Howson Luo <1029143329@qq.com>
2026-03-14 23:17:14 +0800

    test: 测试构建

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      'dayjs/plugin/advancedFormat': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.19/node_modules/dayjs/esm/plugin/advancedFormat/index.js'),
+      'dayjs/plugin/customParseFormat': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.19/node_modules/dayjs/esm/plugin/customParseFormat/index.js'),
+      'dayjs/plugin/localeData': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.19/node_modules/dayjs/esm/plugin/localeData/index.js'),
+      'dayjs/plugin/weekday': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.19/node_modules/dayjs/esm/plugin/weekday/index.js'),
+      'dayjs/plugin/weekOfYear': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.19/node_modules/dayjs/esm/plugin/weekOfYear/index.js'),
+      'dayjs/plugin/weekYear': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.19/node_modules/dayjs/esm/plugin/weekYear/index.js'),
+    },
+  },
+});
