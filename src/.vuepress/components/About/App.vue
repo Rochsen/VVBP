828757484b4e0d7f9324f9b6045afdbf558dec8f
Howson Luo <1029143329@qq.com>
2026-03-14 15:07:44 +0800

    feat: 人格类型完成

--- src/.vuepress/components/About/App.vue
+++ src/.vuepress/components/About/App.vue
@@ -3,9 +3,9 @@
     <!-- 欢迎部分 -->
     <div class="welcome-block">
       <div class="welcome-content">
-        <span style="font-size: 16px">你好，很高兴认识你👋</span>
+        <span style="font-size: 18px">你好，很高兴认识你👋</span>
         <span style="font-size: 30px; font-weight: bold; margin: 10px 0">我叫 罗浩森</span>
-        <span style="font-size: 16px">是一位 生信分析工程师、前/后端开发初学者</span>
+        <span style="font-size: 16px">是一位 生物信息分析/前端开发工程师</span>
       </div>
     </div>
 
@@ -13,10 +13,10 @@
     <div class="personality-type-block">
       <!-- 人格类型说明 -->
       <div class="personality-type-block-content">
-        <div style="display: flex; flex-direction: column;">
-          <span style="color: gray; font-size: 25px;">人格类型</span>
-          <span style="font-size: 50px; font-weight: bold; color: #f0b100">ISTP-A</span>
-          <span style="font-size: 30px; color: #f0b100">鉴赏家</span>
+        <div style="display: flex; flex-direction: column; align-items: center;">
+          <span class="personality-type-name" style="color: gray; font-size: 25px;">人格类型</span>
+          <span class="personality-type-code" style="font-size: 45px; font-weight: bold; color: #f0b100">ISTP-A</span>
+          <span class="personality-type-name" style="font-size: 30px; color: #f0b100">鉴赏家</span>
         </div>
       </div>
       <!-- 人格图片 -->
@@ -25,14 +25,14 @@
       </a>
     </div>
 
-    <!-- 教育背景 -->
-    <div>
-      <div style="font-size: 30px; font-weight: bold; margin: 10px 0">教育背景</div>
-    </div>
-
     <!-- 技术栈部分 -->
     <div>
       <div style="font-size: 30px; font-weight: bold; margin: 10px 0">技术栈</div>
+    </div>
+
+    <!-- 教育背景 -->
+    <div>
+      <div style="font-size: 30px; font-weight: bold; margin: 10px 0">教育背景</div>
     </div>
 
     <!-- 个人职业生涯纪录 -->
@@ -53,72 +53,10 @@
 </template>
 
 <script setup lang="ts">
+import "./index.scss"
 
 defineOptions({
   name: 'AboutPageVue'
 })
 
 </script>
-
-<style scoped>
-/* 欢迎块 */
-.welcome-block {
-  display: flex;
-  align-items: center;
-  background: linear-gradient(40deg, rgb(58, 58, 235), cyan);
-  padding: 20px;
-  border-radius: 10px;
-  height: 150px;
-}
-
-/* 欢迎内容统一样式 */
-.welcome-content {
-  display: flex;
-  flex-direction: column;
-  color: #fff;
-  line-height: 35px;
-}
-
-/* 人格类型块 */
-.personality-type-block {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 225px;
-  background: #fff;
-  border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-}
-
-/* 人格类型块内容统一样式 */
-.personality-type-block-content {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-}
-
-/* 人格图片样式 */
-.istp-img-style {
-  height: 200px;
-  animation: swing 2s ease-in-out infinite;
-}
-
-/* 去掉a标签的after伪类样式 */
-.personality-link::after {
-  display: none !important;
-}
-
-/* 人格类型图片左右摆动动画 */
-@keyframes swing {
-
-  0%,
-  100% {
-    transform: rotate(5deg);
-  }
-
-  50% {
-    transform: rotate(-5deg);
-  }
-}
-</style>
