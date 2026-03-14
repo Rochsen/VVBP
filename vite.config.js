866803d73cf824929a7027a36702200c95828535
Howson Luo <1029143329@qq.com>
2026-03-15 07:48:26 +0800

    test: 修改配置以修复构建

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,18 @@ import path from 'path';
 export default defineConfig({
   resolve: {
     alias: {
-      'dayjs/plugin/advancedFormat.js': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.19/node_modules/dayjs/esm/plugin/advancedFormat/index.js'),
-      'dayjs/plugin/customParseFormat.js': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.19/node_modules/dayjs/esm/plugin/customParseFormat/index.js'),
-      'dayjs/plugin/localeData.js': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.19/node_modules/dayjs/esm/plugin/localeData/index.js'),
-      'dayjs/plugin/weekday.js': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.19/node_modules/dayjs/esm/plugin/weekday/index.js'),
-      'dayjs/plugin/weekOfYear.js': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.19/node_modules/dayjs/esm/plugin/weekOfYear/index.js'),
-      'dayjs/plugin/weekYear.js': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.19/node_modules/dayjs/esm/plugin/weekYear/index.js'),
+      'dayjs/plugin/advancedFormat': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.20/node_modules/dayjs/esm/plugin/advancedFormat/index.js'),
+      'dayjs/plugin/advancedFormat.js': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.20/node_modules/dayjs/esm/plugin/advancedFormat/index.js'),
+      'dayjs/plugin/customParseFormat': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.20/node_modules/dayjs/esm/plugin/customParseFormat/index.js'),
+      'dayjs/plugin/customParseFormat.js': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.20/node_modules/dayjs/esm/plugin/customParseFormat/index.js'),
+      'dayjs/plugin/localeData': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.20/node_modules/dayjs/esm/plugin/localeData/index.js'),
+      'dayjs/plugin/localeData.js': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.20/node_modules/dayjs/esm/plugin/localeData/index.js'),
+      'dayjs/plugin/weekday': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.20/node_modules/dayjs/esm/plugin/weekday/index.js'),
+      'dayjs/plugin/weekday.js': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.20/node_modules/dayjs/esm/plugin/weekday/index.js'),
+      'dayjs/plugin/weekOfYear': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.20/node_modules/dayjs/esm/plugin/weekOfYear/index.js'),
+      'dayjs/plugin/weekOfYear.js': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.20/node_modules/dayjs/esm/plugin/weekOfYear/index.js'),
+      'dayjs/plugin/weekYear': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.20/node_modules/dayjs/esm/plugin/weekYear/index.js'),
+      'dayjs/plugin/weekYear.js': path.resolve(__dirname, 'node_modules/.pnpm/dayjs@1.11.20/node_modules/dayjs/esm/plugin/weekYear/index.js'),
     },
   },
 });
